ee7a87a78a87a01be79abc9a02334808922b257b
Nathan Baledio <45157312+nbaledio@users.noreply.github.com>
2020-08-24 00:57:17 -0700

    Initial Commit

---
+++ Source/BreakThrough/BreakThroughGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BreakThroughGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAKTHROUGH_API ABreakThroughGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
