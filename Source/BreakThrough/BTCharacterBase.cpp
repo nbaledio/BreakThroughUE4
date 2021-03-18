d2817eba7754c08a9d8621c3442d3f982a9feb32
Ryan Santiago <ryansantiago.d7s@gmail.com>
2021-03-18 16:52:08 -0700

    Added a bunch of code, pushbox solver and update character

--- Source/BreakThrough/BTCharacterBase.cpp
+++ Source/BreakThrough/BTCharacterBase.cpp
@@ -9,6 +9,11 @@ ABTCharacterBase::ABTCharacterBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Transform = CreateDefaultSubobject<USceneComponent>(TEXT("Transform"));
+	RootComponent = Transform;
+
+	BaseMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Character Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -25,11 +30,453 @@ void ABTCharacterBase::Tick(float DeltaTime)
 
 void ABTCharacterBase::UpdateCharacter()
 {
+	//Check and Process inputs
+	//Checking PosePlayTime < PlayDuration, Changing Animations, and Anim Transitions
+	//ProcessAnimationFrame();
+	
+	if (HitStop == 0)
+	{
+		if (KnockBack != FVector2D(0, 0)) //apply any knockback
+		{
+			Velocity = FVector2D(Weight * KnockBack.X, Weight * ComboGravity * KnockBack.Y);
 
+			if (bFacingRight)
+			{
+				Velocity *= FVector2D(-1, 1);
+			}
+
+			KnockBack = FVector2D(0, 0);
+		}
+
+		if (SlowMoTime % 2 == 0) //animation speed is halved and stun values decrease at half speed while in slow motion
+		{
+			PosePlayTime++;
+			if (HitStun > 0)
+				HitStun--;
+			if (BlockStun > 0)
+				BlockStun--;
+		}
+
+		if (bIsAirborne && BlitzDashTime == 0) //apply gravity while character is airborne and not defying gravity
+		{
+			float GravCalc = -10.f / 60.f; // GravityScale * -10.f / 60.f;
+
+			if (WallStickTime > 0)
+				GravCalc *= .25f;
+			if (SlowMoTime > 0)
+				GravCalc *= .5f;
+
+			Velocity.Y += GravCalc;
+		}
+		if (!bIsAirborne && Velocity.Y > 0)
+			bIsAirborne = true;
+
+		if (WallStickTime > 0)
+			WallStickTime--;
+
+		if (Opponent != NULL)
+		{
+			if (bTouchingOpponent)
+			{
+				if (bIsRunning && Opponent->bIsRunning)
+				{
+					if (FMath::Abs(Velocity.X) > FMath::Abs(Opponent->Velocity.X))
+					{
+						if (bFacingRight)
+							Velocity.X = FMath::Abs(Velocity.X) - FMath::Abs(Opponent->Velocity.X);
+						else
+							Velocity.X = -(FMath::Abs(Velocity.X) - FMath::Abs(Opponent->Velocity.X));
+					}
+					else
+					{
+						Velocity.X = 0;
+					}
+				}
+				else if (bIsRunning) //opponent provides resistance when dashing against them
+					Velocity.X *= .85f;
+				else if ((bFacingRight && Velocity.X > 0 && Opponent->Velocity.X < 0) || (!bFacingRight && Velocity.X < 0 && Opponent->Velocity.X > 0))
+					Velocity.X = 0;
+			}
+		}
+
+		if (SlowMoTime > 0)
+			Position += Velocity * 100 / 120.f;
+		else
+			Position += Velocity * 100 / 60.f;
+	}
+	else if (HitStop > 0)
+	{
+		HitStop--;
+	}
+
+	if (BlitzDashTime > 0)
+		BlitzDashTime--;
+	if (SlowMoTime > 0)
+		SlowMoTime--;
+
+	SurfaceContact();
 }
 
 void ABTCharacterBase::HitDetection()
 {
 
+}
+
+void ABTCharacterBase::DrawCharacter()
+{
+	if (CurrentAnimFrame != NULL && CurrentAnimFrame->Pose != NULL)
+		BaseMesh->SetAnimation(CurrentAnimFrame->Pose);
+
+	SetActorLocation(FVector(Position.X, GetActorLocation().Y, Position.Y));
+}
+
+void ABTCharacterBase::ProcessAnimationFrame()
+{
+	if (CurrentAnimFrame != NULL)
+	{
+		if (PosePlayTime == 0)
+		{
+			//copy values stored in Animation data to character, these can be overridden by other functions based on character's current circumstances
+			bIsCrouching = CurrentAnimFrame->bIsCrouching;
+			bArmorActive = CurrentAnimFrame->bArmorActive;
+			bCounterHitState = CurrentAnimFrame->bCounterHitState;
+
+			AvailableActions = CurrentAnimFrame->AvailableActions;
+
+			CurrentHitbox = &CurrentAnimFrame->Hitboxes;
+			CurrentUpperBody = &CurrentAnimFrame->HurtboxUpperBody;
+			CurrentLowerBody = &CurrentAnimFrame->HurtboxLowerBody;
+
+			if (CurrentHitbox->Num() > 0)
+				if ((*CurrentHitbox)[0].bNewHit)
+					bAttackMadeContact = false;
+
+			if (CurrentAnimFrame->Acceleration != FVector2D(0, 0) || CurrentAnimFrame->bStopVelocityY || CurrentAnimFrame->bStopVelocityX)
+			{
+				if (bFacingRight)
+				{
+					Velocity = FVector2D((!CurrentAnimFrame->bStopVelocityX * Velocity.X) + CurrentAnimFrame->Acceleration.X, 
+						(!CurrentAnimFrame->bStopVelocityY * Velocity.Y) + CurrentAnimFrame->Acceleration.Y);
+				}
+				else
+				{
+					Velocity = FVector2D((!CurrentAnimFrame->bStopVelocityX * Velocity.X) - CurrentAnimFrame->Acceleration.X,
+						(!CurrentAnimFrame->bStopVelocityY * Velocity.Y) + CurrentAnimFrame->Acceleration.Y);
+				}
+			}
+			if (bIsAirborne && CurrentAnimFrame->LandingLag > 0)
+				LandingLag = CurrentAnimFrame->LandingLag + 4;
+		}
+	}
+}
+
+void ABTCharacterBase::EnterNewAnimation(TArray<FAnimationFrame> Animation, int32 FrameNumber)
+{
+	//check if the specified animation and frame exist
+	if (Animation.Num() > FrameNumber)
+	{
+		if (Animation[FrameNumber].Pose != NULL)
+		{
+			PosePlayTime = 0; // reset pose play time to make sure new frame is shown for the correct amount of time
+			CurrentAnimation = &Animation;
+			CurrentAnimFrame = &Animation[FrameNumber];
+		}
+	}
+}
+
+void ABTCharacterBase::TurnAroundCheck()
+{
+	if (Opponent != NULL)
+	{
+		if (Opponent->GetActorLocation().X < GetActorLocation().X)
+			bFacingRight = false;
+		else if (Opponent->GetActorLocation().X > GetActorLocation().X)
+			bFacingRight = true;
+	}
+
+	if (!bFacingRight && Transform->GetRelativeScale3D().X != -1)
+	{
+		Transform->SetRelativeScale3D(FVector(-1, 1, 1));
+		TriggerTurnAround();
+	}
+	else if (bFacingRight && Transform->GetRelativeScale3D().X != 1)
+	{
+		Transform->SetRelativeScale3D(FVector(1, 1, 1));
+		TriggerTurnAround();
+	}
+}
+
+void ABTCharacterBase::TriggerTurnAround()
+{
+	//Trigger turnaround animation if in idle stand or crouch;
+	if (AvailableActions & AcceptMove && !bIsAirborne && Velocity.X == 0)
+	{
+		PosePlayTime = 0;
+		if (bIsCrouching)
+		{
+			EnterNewAnimation(TurnAroundCrouch);
+		}
+		else if (TurnAroundStand.Num() > 0)
+		{
+			EnterNewAnimation(TurnAroundStand);
+		}
+	}
+}
+
+void ABTCharacterBase::SurfaceContact()
+{
+	if (LandingLag > 0) //landing lag system
+	{
+		if (LandingLag > 4)
+		{
+			bCounterHitState = true;
+			bIsCrouching = true;
+
+			if (AvailableActions & AcceptBlitz)
+				AvailableActions = AcceptBlitz + AcceptSuper;
+			else
+				AvailableActions = None;
+		}
+		else //universal four frames of recovery upon landing on feet that only restricts movement
+		{
+			bCounterHitState = false;
+			AvailableActions = AcceptAll - (AcceptMove + AcceptJump);
+		}
+
+		if (!bIsAirborne)
+		{
+			LandingLag--;
+		}
+	}
+
+	if (bIsAirborne && Position.Y <= 0 && Velocity.Y <= 0 && CurrentAnimFrame != NULL) //landing on the ground
+	{
+		Position.Y = 0;
+		//logic for setting character's state when they hit the ground (land on feet, knockdown, groundbounce)
+		if (CharacterHitState & GroundBounce)
+		{
+			//cause ground bounce and ground bounce animation
+			CharacterHitState -= GroundBounce;
+		}
+		else
+		{
+			Velocity.Y = 0;
+			bIsAirborne = false;
+			PosePlayTime = 0;
+			if (CurrentAnimFrame->Invincibility != FaceDown) //cause hard knockdown if in a hitstun state
+			{
+				EnterNewAnimation(KnockDownFaceDown);
+			}
+			else if (CurrentAnimFrame->Invincibility != FaceUp)
+			{
+				EnterNewAnimation(KnockDownFaceUp);
+			}
+			else //if a character is not in a hit state, they will land on their feet
+			{
+				if (LandingLag > 4) //landing lag puts character in a recovery state when landing
+				{
+					EnterNewAnimation(CrouchDown);
+					bIsCrouching = true;
+				}
+				else
+				{
+					EnterNewAnimation(StandUp);
+					LandingLag = 4;
+				}
+			}
+		}
+	}
+	
+	//Check if hitting wall, 20 meters is the tentative stage length, subject to change
+	if (Position.X <= -10 + .5f * PushboxWidth)
+	{
+		Position.X = -10 + .5f * PushboxWidth;
+		HitWall();
+	}
+	else if (Position.X >= 10 - .5f * PushboxWidth)
+	{
+		Position.X = 10 - .5f * PushboxWidth;
+		HitWall();
+	}
+	else
+	{
+		bTouchingWall = false;
+	}
+}
+
+void ABTCharacterBase::HitWall()
+{
+	bTouchingWall = true;
+	if (CharacterHitState & WallStick)
+	{
+		//cause wall stick
+		WallStickTime = 30;
+		CharacterHitState = None;
+		//minimum Position.Y
+	}
+	else if (CharacterHitState & WallBounce)
+	{
+		//cause wall bounce and wall bounce animation
+		CharacterHitState -= WallBounce;
+		//reverse Velocity.X and minimum Velocity.X
+		//minimum Velocity.Y
+		return;
+	}
+
+	Velocity.X = 0;
+}
+
+void ABTCharacterBase::Guarding()
+{
+	if (AvailableActions & AcceptGuard)
+	{
+		if (!bIsAirborne && (Dir1 >= InputTime - 1 || Dir2 >= InputTime - 1 || Dir3 >= InputTime - 1))
+		{
+			//on the ground and holding a downward direction means the character is crouching
+			bIsCrouching = true;
+		}
+
+		if (Dir7 >= InputTime - 1 || Dir4 >= InputTime - 1 || Dir1 >= InputTime - 1)
+		{
+			//holding a backward direction while able to guard keeps the character's guard up
+			bIsGuarding = true;
+		}
+	}
+}
+
+void ABTCharacterBase::PushboxSolver() //only called once per gamestate tick after updating characters, do not call on multiple characters
+{
+	if (Opponent != NULL)
+	{
+		if (CurrentAnimFrame != NULL && Opponent->CurrentAnimFrame != NULL)
+		{
+			if (CurrentAnimFrame->Invincibility != Intangible && Opponent->CurrentAnimFrame->Invincibility != Intangible && 
+				FMath::Abs(Opponent->Position.X - Position.X) <= .5f * Opponent->PushboxWidth + .5f * PushboxWidth)
+			{
+				if (!bIsAirborne && !Opponent->bIsAirborne) //both on the ground
+				{
+					if (bTouchingWall)
+					{
+						if (bFacingRight)
+							Opponent->Position.X = Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+						else
+							Opponent->Position.X = Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+					}
+					else if (Opponent->bTouchingWall)
+					{
+						if (bFacingRight)
+							Position.X = Opponent->Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+						else
+							Position.X = Opponent->Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+					}
+					//when character is faster than opponent, and both are moving in the same direction or towards each other
+					else if (FMath::Abs(Velocity.X) > FMath::Abs(Opponent->Velocity.X) && ((Velocity.X < 0 && Opponent->Velocity.X < 0) || (Velocity.X > 0 && Opponent->Velocity.X > 0) ||
+					(!bFacingRight && Velocity.X < 0 && Opponent->Velocity.X > 0) || (bFacingRight && Velocity.X > 0 && Opponent->Velocity.X < 0)))
+					{
+						if (bFacingRight)
+						{
+							Opponent->Position.X = Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+
+							if (Opponent->Position.X >= 10 - .5f * Opponent->PushboxWidth)
+							{
+								Opponent->bTouchingWall = true;
+								Opponent->Velocity.X = 0;
+								Opponent->Position.X = -10 + .5f * Opponent->PushboxWidth;
+								Position.X = Opponent->Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+								Velocity.X = 0;
+							}
+						}
+						else
+						{
+							Opponent->Position.X = Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+
+							if (Opponent->Position.X <= -10 + .5f * Opponent->PushboxWidth)
+							{
+								Opponent->bTouchingWall = true;
+								Opponent->Velocity.X = 0;
+								Opponent->Position.X = -10 + .5f * Opponent->PushboxWidth;
+								Position.X = Opponent->Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+								Velocity.X = 0;
+							}
+						}
+					}
+					//when opponent is faster than character, and both are moving in the same direction or towards each other
+					else if (FMath::Abs(Velocity.X) < FMath::Abs(Opponent->Velocity.X) && ((Velocity.X < 0 && Opponent->Velocity.X < 0) || (Velocity.X > 0 && Opponent->Velocity.X > 0) ||
+					(!bFacingRight && Velocity.X < 0 && Opponent->Velocity.X > 0) || (bFacingRight && Velocity.X > 0 && Opponent->Velocity.X < 0)))
+					{
+						if (bFacingRight)
+						{
+							Position.X = Opponent->Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+
+							if (Position.X <= -10 + .5f * PushboxWidth)
+							{
+								bTouchingWall = true;
+								Velocity.X = 0;
+								Position.X = -10 + .5f * PushboxWidth;
+								Opponent->Position.X = Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+								Opponent->Velocity.X = 0;
+							}
+						}
+						else
+						{
+							Position.X = Opponent->Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+
+							if (Position.X >= 10 - .5f * PushboxWidth)
+							{
+								bTouchingWall = true;
+								Velocity.X = 0;
+								Position.X = 10 - .5f * PushboxWidth;
+								Opponent->Position.X = Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+								Opponent->Velocity.X = 0;
+							}
+						}
+					}
+					else //if both characters have the same velocity or arent moving
+					{
+						float MoveDistance = .5f * (.5f * Opponent->PushboxWidth + .5f * PushboxWidth - FMath::Abs(Opponent->Position.X - Position.X));
+
+						if (bFacingRight)
+						{
+							Position.X -= MoveDistance;
+							Opponent->Position.X += MoveDistance;
+						}
+						else
+						{
+							Position.X += MoveDistance;
+							Opponent->Position.X -= MoveDistance;
+						}
+					}
+					bTouchingOpponent = true;
+					Opponent->bTouchingOpponent = true;
+				}
+				else if (!bIsAirborne && Opponent->bIsAirborne && Opponent->Velocity.Y < 0)
+				{
+					if ((bIsCrouching && CrouchingPushBoxHeight > Opponent->Position.Y + Opponent->AirPushboxVerticalOffset) ||
+							(!bIsCrouching && StandingPushBoxHeight > Opponent->Position.Y + Opponent->AirPushboxVerticalOffset)) //check if pushboxes intersect
+					{
+						if (bTouchingWall)
+						{
+							if (bFacingRight)
+								Opponent->Position.X = Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+							else
+								Opponent->Position.X = Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+						}
+						else if (Opponent->bTouchingWall)
+						{
+							if (bFacingRight)
+								Position.X = Opponent->Position.X - (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+							else
+								Position.X = Opponent->Position.X + (.5f * Opponent->PushboxWidth + .5f * PushboxWidth);
+						}
+					}
+				}
+				else
+				{
+					bTouchingOpponent = false;
+					Opponent->bTouchingOpponent = false;
+				}
+			}
+		}
+	}
 }
 
--- Source/BreakThrough/BTCharacterBase.h
+++ Source/BreakThrough/BTCharacterBase.h
@@ -24,13 +24,18 @@ enum GuardHeight
 
 enum SpecialStates
 {
-	ProjectileInvincible = 1,
+	None = 0,
+	ProjectileInvincible,
 	StrikeInvincible,
 	ThrowInvincible,
 	FullInvincible,
+	Intangible,
 	HiCounter,
 	LowCounter,
-	AllCounter
+	AllCounter,
+	FaceDown,
+	FaceUp,
+	OTG // 30% damage and halved hitstun
 };
 
 enum CharacterActions
@@ -51,19 +56,20 @@ enum CharacterActions
 
 enum AttackProperties
 {
-	None = 0,
-	GroundBounce = (1 << 0),
-	WallBounce = (1 << 1),
-	WallStick = (1 << 2),
-	Sweep = (1 << 3),
-	Launch = (1 << 4),
-	Stagger = (1 << 5),
-	Crumple = (1 << 6),
-	KnockAway = (1 << 7),
-	Deflected = (1 << 8),
-	Tumbling = (1 << 9),
+	GroundBounce = (1 << 0), //can be bounced against the ground
+	WallBounce = (1 << 1), //can be bounced off of walls
+	WallStick = (1 << 2), //can be stuck against walls
+	Sweep = (1 << 3), //sweep: hitstun does not decrement until in fallingforward animation
+	Launch = (1 << 4), //launch: no special properties, purely aesthetic
+	Stagger = (1 << 5), //stagger: can still be thrown despite being a hitstun state, need to hold button to recover back to standing position once hitstun ends
+	Crumple = (1 << 6), //crumple: long hitstun state with preset duration, ends in facedown knockdown, can still be thrown despite being a hitstun state
+	KnockAway = (1 << 7), //knock away: no special properties, purely aesthetic
+	Deflected = (1 << 8), //deflected: hitstun state with preset duration, can still be thrown
+	Tumbling = (1 << 9), //tumbling: an airborne hitstun state that cannot be air recovered from
 	IsSpecial = (1 << 10),
 	IsSuper = (1 << 11),
+	IsSlash = (1 << 12),
+	IsVertical = (1 << 13),
 };
 
 USTRUCT(BlueprintType)
@@ -102,6 +108,8 @@ struct FHitbox
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
 		int32 ResolveDamage = 0;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+		int32 AttackLevel = 0;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
 		int32 AttackHeight = Mid;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
 		int32 BaseHitStun = 0;
@@ -128,9 +136,24 @@ struct FAnimationFrame
 		UAnimationAsset* Pose;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
 		int32 PlayDuration;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+		bool bDoesCycle;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+		bool bSuperFlash; //freezes Opposing character in place
+
+	//relinquish complete control of camera positioning to animation/character class, camera will snap to stored positions, setting this to true automatically switches on bSuperFlash as well
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+		bool bCinematic;
+
+	//camera location relative to character's position, will snap to location if bCinematic is also on, otherwise will lerp to new positions, must be a non zero vector to use
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+		FVector CameraLocation;
+	//camera rotation, will snap to rotation if bCinematic is also on, otherwise will lerp to new rotation
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+		FRotator CameraRotation;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Hitboxes")
-		TArray<FHurtbox> Hitboxes;
+		TArray<FHitbox> Hitboxes;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Hurtboxes")
 		TArray<FHurtbox> HurtboxUpperBody;
@@ -140,7 +163,16 @@ struct FAnimationFrame
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
 		int32 AvailableActions; //uses CharacterActions enum, determines what actions are available to the player upon entering this animation frame
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
-		int32 Invincibility; //uses SpecialStates enum
+		int32 Invincibility; //uses SpecialStates enum, determines what attacks can hit the character, used to dictate a face up or face down knockdown on airborne hitstun states
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+		bool bIsCrouching = false;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+		bool bArmorActive = false;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+		bool bCounterHitState = false;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+		int32 LandingLag = 0;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
 		bool bStopVelocityX;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
@@ -165,9 +197,26 @@ public:
 
 	virtual void HitDetection();
 
+	virtual void DrawCharacter();
+
+	ABTCharacterBase* Opponent;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay();
+
+	TArray<FAnimationFrame>* CurrentAnimation;
+	FAnimationFrame* CurrentAnimFrame;
+	TArray<FHitbox>* CurrentHitbox;
+	TArray<FHurtbox>* CurrentUpperBody;
+	TArray<FHurtbox>* CurrentLowerBody;
+
+	int32 AnimFrameIndex;
+	int32 PosePlayTime = 0;
+
+	TArray<int32> Inputs;
+	int32 Seals = 0;
+	int32 SealTimer = 0;
 
 	//character's vitality, loses when it hits zero
 	UPROPERTY(VisibleAnywhere, Category = "Battle Stats")
@@ -183,8 +232,8 @@ protected:
 
 	//values that represent a character's resilience as a battle rages on
 	//scales down damage received based on how low the character's health is
-	UPROPERTY(EditDefaultsOnly, Category = "Battle Stats")
-		float CurrentValor;
+	/*UPROPERTY(VisibleAnywhere, Category = "Battle Stats")
+		float CurrentValor;*/
 	UPROPERTY(EditDefaultsOnly, Category = "Battle Stats")
 		float Valor100;
 	UPROPERTY(EditDefaultsOnly, Category = "Battle Stats")
@@ -211,30 +260,50 @@ protected:
 		int32 ShatteredTime = 0;
 	UPROPERTY(VisibleAnywhere, Category = "Battle Stats")
 		int32 SlowMoTime = 0;
+	UPROPERTY(VisibleAnywhere, Category = "Battle Stats")
+		int32 WallStickTime = 0;
 
 	UPROPERTY(VisibleAnywhere, Category = "Battle Stats")
 		int32 JumpsUsed = 0;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
-		float AnimSpeed = 1.f;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
 		bool bIsAirborne = true;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
 		bool bIsCrouching = false;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
-		bool bIsGuarding = true;
+		bool bIsGuarding = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
+		bool bTouchingWall = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Battle Stats")
+		bool bTouchingOpponent = false;
 
+	/* A flat knockback scale that is applied to all knockback applied to the character (See below for values per weight class)
+		Featherweight = 1.05 
+		Lightweight = 1.02
+		Middleweight = 1
+		Heavyweight = .97  */
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
 		float Weight = 1;
+	/*UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
+		float GravityScale = 1;*/
+	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
+		float StandingPushBoxHeight = 1;
+	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
+		float CrouchingPushBoxHeight = 1;
+	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
+		float PushboxWidth = 1;
+	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
+		float AirPushboxVerticalOffset = 0;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
 		float WalkSpeed = 1;
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
 		float WalkBackSpeed = 1;
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
-		float RunAcceleration = .2f;
+		float RunAcceleration = .2f; //set RunAcceleration to zero to disable run/dash
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
-		float MaxRunSpeed = 2;
+		float MaxRunSpeed = 2; //set MaxRunSpeed to zero to enable dash type
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
 		float BlitzDashForce = 2;
 	UPROPERTY(EditDefaultsOnly, Category = "Movement Properties")
@@ -247,19 +316,20 @@ protected:
 		FVector2D JumpForce;
 
 	UPROPERTY(VisibleAnywhere, Category = "Movement Properties")
-		FVector2D Position;
+		FVector2D Position; // Y = 0 is considered the ground, X = (-/+)10 are the left and right walls respectively
 	UPROPERTY(VisibleAnywhere, Category = "Movement Properties")
 		FVector2D Velocity;
 
 	UPROPERTY(VisibleAnywhere, Category = "Movement Properties") //keeps track of non-movement acceleration to apply once hitstop is zero
 		FVector2D KnockBack;
 
-	//the influence of gravity on a character
-	//if being hit, increases with the number of hits in a combo to make continuing a combo more difficult as time goes on
-		float GravityScale = 1;
+	//value that increasingly scales positive vertical knockback the longer a character is in a combo
+	//causes a character to not be launched as high the more hits there are in a combo
+		float ComboGravity = 1;
+		int ComboCount = 0;
 
 	//number of frames that an input is active for
-		int32 InputTime = 12;
+		int32 InputTime = 10;
 	// ints to denote active time on directional inputs
 		int32 Dir1 = 0;
 		int32 Dir2 = 0;
@@ -298,9 +368,7 @@ protected:
 		bool bIsRunning = false;
 		bool bArmorActive = false;
 		bool bCounterHitState = false;
-		bool bTouchingWall = false;
-		int32 CharacterHitState = None; //used if an attack puts character in a specific animation state (crumple, sweep, launch, etc.), or if they can be bounced against/ stuck to surfaces
-		int32 Invincibility = None; // determines if a character is invincible, projectile immune, deflects attacks of certain heights, etc
+		int32 CharacterHitState = None; //determines if character should be in a specific animation state (crumple, sweep, launch, etc.), or if they can be bounced against/ stuck to surfaces, uses AttackProperties enum
 
 	//keeps track of whether an attack has already hit something
 	//attack effects are only applied based on the first overlap interaction with the attack (!bAttackMadeContact)
@@ -308,12 +376,58 @@ protected:
 	//keeps track if an attack makes a hit, used for attacks that have a followup when they hit
 		bool bHitSuccess = false;
 
+	//Idle Stance Animations
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> TurnAroundCrouch;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> TurnAroundStand;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> CrouchDown;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> StandUp;
+
+	//Knockdown/WakeUp Animations
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> KnockDownFaceDown;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animations")
+		TArray<FAnimationFrame> KnockDownFaceUp;
+
 private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* Transform;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		USkeletalMeshComponent* BaseMesh;
 
+	//Take in information from CurrentAnimFrame
+	void ProcessAnimationFrame();
+
+	void EnterNewAnimation(TArray<FAnimationFrame> Animation, int32 FrameNumber = 0);
+
+	void TurnAroundCheck();
+
+	void TriggerTurnAround();
+
+	void SurfaceContact();
+
+	void HitWall();
+
+	void Guarding();
+
+	void PushboxSolver(); //only called once by gamestate, do not call for each character
 };
 
 /* 
- TArray<AnimationPose> StandingAnim
+ Basic Gamestate Tick:
+ P1->HitDetection();
+ p2->HitDetection(); //possible to change animations from here
+
+ P1->UpdateCharacter(); //process inputs, transition to new animation if required and process new animation data at start of update
+ P2->UpdateCharacter();
+
+ P1->PushboxSolver();
+
+ P1->DrawCharacter();
+ P2->DrawCharacter();
+ GameState->Timers and stuff, update UI
 */
