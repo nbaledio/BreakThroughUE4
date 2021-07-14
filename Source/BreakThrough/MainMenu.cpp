d74f6e9846d8a69ec30bbfff106f2ca8340f2410
Nathan Baledio <45157312+nbaledio@users.noreply.github.com>
2021-07-13 20:23:00 -0700

    Finished Main Menu base functionality

---
+++ Source/BreakThrough/MainMenu.cpp
@@ -0,0 +1,587 @@
+// Copyright 2020 ShatterPoint Games. All Rights Reserved.
+
+
+#include "MainMenu.h"
+
+void UMainMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	//Set controllers
+	P1Controller = UGameplayStatics::GetPlayerController((UObject*)GetWorld(), 0);
+	P2Controller = UGameplayStatics::GetPlayerController((UObject*)GetWorld(), 0);
+
+	//Play Animations
+	PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	PlayAnimation(FlowerAnimation, 0.0f, 0, EUMGSequencePlayMode::Forward, 0.1f, false);
+	PlayAnimation(CloudAnimation, 0.0f, 0, EUMGSequencePlayMode::Forward, 0.01f, false);
+}
+
+void UMainMenu::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	//Read P1 inputs
+	if (P1Controller)
+	{
+		GetP1Inputs();
+	}
+
+	//Read P2Inputs
+	if (P2Controller)
+	{
+		GetP2Inputs();
+	}
+
+	//Set menu interactions based on current menu
+	switch (menuIndex) 
+	{
+	case 0:
+		MainMenuInteractions();
+		break;
+	case 1:
+		PlayLocalMenuInteractions();
+		break;
+	case 2:
+		OptionsMenuInteractions();
+		break;
+	case 3:
+		SideSelectMenuInteractions();
+		break;
+	}
+}
+
+void UMainMenu::GetP1Inputs()
+{
+	//Set bool variables
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Down) || P1Controller->IsInputKeyDown(EKeys::S))
+	{
+		P1_INPUT_DOWN = true;
+	}
+	else
+	{
+		P1_INPUT_DOWN = false;
+	}
+
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Up) || P1Controller->IsInputKeyDown(EKeys::W))
+	{
+		P1_INPUT_UP = true;
+	}
+	else
+	{
+		P1_INPUT_UP = false;
+	}
+
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Left) || P1Controller->IsInputKeyDown(EKeys::A))
+	{
+		P1_INPUT_LEFT = true;
+	}
+	else
+	{
+		P1_INPUT_LEFT = false;
+	}
+
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Right) || P1Controller->IsInputKeyDown(EKeys::D))
+	{
+		P1_INPUT_RIGHT = true;
+	}
+	else
+	{
+		P1_INPUT_RIGHT = false;
+	}
+
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_FaceButton_Right) || P1Controller->IsInputKeyDown(EKeys::K))
+	{
+		P1_INPUT_BACK = true;
+	}
+	else
+	{
+		P1_INPUT_BACK = false;
+	}
+
+	if (P1Controller->IsInputKeyDown(EKeys::Gamepad_FaceButton_Bottom) || P1Controller->IsInputKeyDown(EKeys::J))
+	{
+		P1_INPUT_ACCEPT = true;
+	}
+	else
+	{
+		P1_INPUT_ACCEPT = false;
+	}
+
+	//Read P1 analog inputs
+	P1Controller->GetInputAnalogStickState(EControllerAnalogStick::CAS_LeftStick, P1_HORIZONTAL_AXIS, P1_VERTICAL_AXIS);
+
+	//Calculate P1 DPad inputs
+	if (P1_INPUT_LEFT)
+	{
+		P1_HORIZONTAL_AXIS -= 1.0f;
+		if (P1_HORIZONTAL_AXIS < -1.0f)
+		{
+			P1_HORIZONTAL_AXIS = -1.0f;
+		}
+	}
+	if (P1_INPUT_RIGHT)
+	{
+		P1_HORIZONTAL_AXIS += 1.0f;
+		if (P1_HORIZONTAL_AXIS > 1.0f)
+		{
+			P1_HORIZONTAL_AXIS = 1.0f;
+		}
+	}
+	if (P1_INPUT_UP)
+	{
+		P1_VERTICAL_AXIS += 1.0f;
+		if (P1_VERTICAL_AXIS > 1.0f)
+		{
+			P1_VERTICAL_AXIS = 1.0f;
+		}
+	}
+	if (P1_INPUT_DOWN)
+	{
+		P1_VERTICAL_AXIS -= 1.0f;
+		if (P1_VERTICAL_AXIS < -1.0f)
+		{
+			P1_VERTICAL_AXIS = -1.0f;
+		}
+	}
+}
+
+void UMainMenu::GetP2Inputs()
+{
+
+	if (P2Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Left) || P2Controller->IsInputKeyDown(EKeys::Left))
+	{
+		P2_INPUT_LEFT = true;
+	}
+	else
+	{
+		P2_INPUT_LEFT = false;
+	}
+
+	if (P2Controller->IsInputKeyDown(EKeys::Gamepad_DPad_Right) || P2Controller->IsInputKeyDown(EKeys::Right))
+	{
+		P2_INPUT_RIGHT = true;
+	}
+	else
+	{
+		P2_INPUT_RIGHT = false;
+	}
+
+	//Read P2 analog inputs
+	P2Controller->GetInputAnalogStickState(EControllerAnalogStick::CAS_LeftStick, P2_HORIZONTAL_AXIS, P2_VERTICAL_AXIS);
+
+	//Calculate P2 DPad inputs
+	if (P2_INPUT_LEFT)
+	{
+		P2_HORIZONTAL_AXIS -= 1.0f;
+		if (P2_HORIZONTAL_AXIS < -1.0f)
+		{
+			P2_HORIZONTAL_AXIS = -1.0f;
+		}
+	}
+	if (P2_INPUT_RIGHT)
+	{
+		P2_HORIZONTAL_AXIS += 1.0f;
+		if (P2_HORIZONTAL_AXIS > 1.0f)
+		{
+			P2_HORIZONTAL_AXIS = 1.0f;
+		}
+	}
+}
+
+void UMainMenu::MainMenuInteractions() 
+{
+	//Scroll through Options
+	if (P1_INPUT_DOWN && P1AcceptVerticalInput) 
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex++;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (P1_INPUT_UP && P1AcceptVerticalInput) 
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex--;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if(!P1_INPUT_DOWN && !P1_INPUT_UP)
+	{
+		P1AcceptVerticalInput = true;
+	}
+
+	//Loop option index
+	if (optionIndex > 3)
+	{
+		optionIndex = 0;
+	}
+	else if (optionIndex < 0)
+	{
+		optionIndex = 3;
+	}
+
+	//Check for confirm input
+	if (P1_INPUT_ACCEPT && P1AcceptConfirmInput)
+	{
+		P1AcceptConfirmInput = false;
+		switch (optionIndex) 
+		{
+		case 0:
+			PlayLocalMenu->SetVisibility(ESlateVisibility::Visible);
+			MainMenu->SetVisibility(ESlateVisibility::Hidden);
+			menuIndex = 1;
+			optionIndex = 0;
+			break;
+		case 1:
+			//Enter online mode
+			break;
+		case 2:
+			menuIndex = 2;
+			OptionsMenu->SetVisibility(ESlateVisibility::Visible);
+			MainMenu->SetVisibility(ESlateVisibility::Hidden);
+			optionIndex = 0;
+			break;
+		case 3:
+			//Return to title
+			break;
+		}
+	}
+	else if (!P1_INPUT_ACCEPT) 
+	{
+		P1AcceptConfirmInput = true;
+	}
+
+	//Set marker
+	SetMarker(optionIndex);
+}
+
+void UMainMenu::PlayLocalMenuInteractions()
+{
+	//Scroll through Options
+	if (P1_INPUT_DOWN && P1AcceptVerticalInput)
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex++;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (P1_INPUT_UP && P1AcceptVerticalInput)
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex--;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (!P1_INPUT_DOWN && !P1_INPUT_UP)
+	{
+		P1AcceptVerticalInput = true;
+	}
+
+	//Loop option index
+	if (optionIndex > 4)
+	{
+		optionIndex = 0;
+	}
+	else if (optionIndex < 0)
+	{
+		optionIndex = 4;
+	}
+
+	//Check for confirm input
+	if (P1_INPUT_ACCEPT && P1AcceptConfirmInput)
+	{
+		P1AcceptConfirmInput = false;
+		switch (optionIndex)
+		{
+		case 0:
+			SideSelectMenu->SetVisibility(ESlateVisibility::Visible);
+			P1ControllerIcon->SetVisibility(ESlateVisibility::Visible);
+			P2ControllerIcon->SetVisibility(ESlateVisibility::Visible);
+			P1CPUText->SetVisibility(ESlateVisibility::Hidden);
+			P2CPUText->SetVisibility(ESlateVisibility::Hidden);
+			PlayAnimation(ResetControllerPositions, 0.0f, 1, EUMGSequencePlayMode::Reverse, 1.0f, false);
+			Gamemode = "VS";
+			menuIndex = 3;
+			break;
+		case 1:
+			SideSelectMenu->SetVisibility(ESlateVisibility::Visible);
+			P1ControllerIcon->SetVisibility(ESlateVisibility::Visible);
+			P2ControllerIcon->SetVisibility(ESlateVisibility::Hidden);
+			P1CPUText->SetVisibility(ESlateVisibility::Visible);
+			P2CPUText->SetVisibility(ESlateVisibility::Visible);
+			menuIndex = 3;
+			Gamemode = "CPU";
+			break;
+		case 2:
+			SideSelectMenu->SetVisibility(ESlateVisibility::Visible);
+			P1ControllerIcon->SetVisibility(ESlateVisibility::Visible);
+			P2ControllerIcon->SetVisibility(ESlateVisibility::Hidden);
+			P1CPUText->SetVisibility(ESlateVisibility::Visible);
+			P2CPUText->SetVisibility(ESlateVisibility::Visible);
+			menuIndex = 3;
+			Gamemode = "Training";
+			break;
+		case 3:
+			//Enter Tutorial Mode
+			break;
+		case 4:
+			PlayLocalMenu->SetVisibility(ESlateVisibility::Hidden);
+			MainMenu->SetVisibility(ESlateVisibility::Visible);
+			menuIndex = 0;
+			optionIndex = 0;
+			PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+			break;
+		}
+	}
+	else if (!P1_INPUT_ACCEPT)
+	{
+		P1AcceptConfirmInput = true;
+	}
+
+	//Check for back input
+	if (P1_INPUT_BACK && P1AcceptBackInput) 
+	{
+		P1AcceptBackInput = false;
+		PlayLocalMenu->SetVisibility(ESlateVisibility::Hidden);
+		MainMenu->SetVisibility(ESlateVisibility::Visible);
+		menuIndex = 0;
+		optionIndex = 0;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (!P1_INPUT_BACK) 
+	{
+		P1AcceptBackInput = true;
+	}
+
+	//Set marker
+	SetMarker(optionIndex);
+}
+
+void UMainMenu::OptionsMenuInteractions()
+{
+	//Scroll through Options
+	if (P1_INPUT_DOWN && P1AcceptVerticalInput)
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex++;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (P1_INPUT_UP && P1AcceptVerticalInput)
+	{
+		P1AcceptVerticalInput = false;
+		optionIndex--;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (!P1_INPUT_DOWN && !P1_INPUT_UP)
+	{
+		P1AcceptVerticalInput = true;
+	}
+
+	//Loop option index
+	if (optionIndex > 3)
+	{
+		optionIndex = 0;
+	}
+	else if (optionIndex < 0)
+	{
+		optionIndex = 3;
+	}
+
+	//Check for confirm input
+	if (P1_INPUT_ACCEPT && P1AcceptConfirmInput)
+	{
+		P1AcceptConfirmInput = false;
+		switch (optionIndex)
+		{
+		case 0:
+			//Open open sound options
+			break;
+		case 1:
+			//Open graphics options
+			break;
+		case 2:
+			//Open controller settings
+			break;
+		case 3:
+			OptionsMenu->SetVisibility(ESlateVisibility::Hidden);
+			MainMenu->SetVisibility(ESlateVisibility::Visible);
+			menuIndex = 0;
+			optionIndex = 2;
+			break;
+		}
+	}
+	else if (!P1_INPUT_ACCEPT)
+	{
+		P1AcceptConfirmInput = true;
+	}
+
+	//Check for back input
+	if (P1_INPUT_BACK && P1AcceptBackInput)
+	{
+		P1AcceptBackInput = false;
+		OptionsMenu->SetVisibility(ESlateVisibility::Hidden);
+		MainMenu->SetVisibility(ESlateVisibility::Visible);
+		menuIndex = 0;
+		optionIndex = 2;
+		PlayAnimation(MarkerAnimation, 0.0f, 1, EUMGSequencePlayMode::Forward, 5.0f, false);
+	}
+	else if (!P1_INPUT_BACK)
+	{
+		P1AcceptBackInput = true;
+	}
+
+	//Set marker
+	SetMarker(optionIndex);
+}
+
+void UMainMenu::SideSelectMenuInteractions()
+{
+	//Check for P1 Input
+	if (P1_INPUT_LEFT && P1AcceptHorizontalInput && !loading) 
+	{
+		P1AcceptHorizontalInput = false;
+		if (P1Side > -1 && !(P2Side == -1 && P1Side == 0))
+		{
+			P1Side--;
+			if (P1Side == -1) 
+			{
+				StopAnimation(P1CToL);
+				StopAnimation(P1CToR);
+				PlayAnimation(P1CToL, 0.0f, 1, EUMGSequencePlayMode::Forward, 8.0f, false);
+			}
+			else if (P1Side == 0) 
+			{
+				StopAnimation(P1CToL);
+				StopAnimation(P1CToR);
+				PlayAnimation(P1CToR, 0.0f, 1, EUMGSequencePlayMode::Reverse, 8.0f, false);
+			}
+		}
+	}
+	else if (P1_INPUT_RIGHT && P1AcceptHorizontalInput && !loading)
+	{
+		P1AcceptHorizontalInput = false;
+		if (P1Side < 1 && !(P2Side == 1 && P1Side == 0))
+		{
+			P1Side++;
+			if (P1Side == 1)
+			{
+				StopAnimation(P1CToL);
+				StopAnimation(P1CToR);
+				PlayAnimation(P1CToR, 0.0f, 1, EUMGSequencePlayMode::Forward, 8.0f, false);
+			}
+			else if (P1Side == 0)
+			{
+				StopAnimation(P1CToL);
+				StopAnimation(P1CToR);
+				PlayAnimation(P1CToL, 0.0f, 1, EUMGSequencePlayMode::Reverse, 8.0f, false);
+			}
+		}
+	}
+	else if (!P1_INPUT_LEFT && !P1_INPUT_RIGHT) 
+	{
+		P1AcceptHorizontalInput = true;
+	}
+
+	if (Gamemode == "VS" && !loading) 
+	{
+		//Check for P2 Input
+		if (P2_INPUT_LEFT && P2AcceptHorizontalInput)
+		{
+			P2AcceptHorizontalInput = false;
+			if (P2Side > -1 && !(P1Side == -1 && P2Side == 0))
+			{
+				P2Side--;
+				if (P2Side == -1)
+				{
+					StopAnimation(P2CToL);
+					StopAnimation(P2CToR);
+					PlayAnimation(P2CToL, 0.0f, 1, EUMGSequencePlayMode::Forward, 8.0f, false);
+				}
+				else if (P2Side == 0)
+				{
+					StopAnimation(P2CToL);
+					StopAnimation(P2CToR);
+					PlayAnimation(P2CToR, 0.0f, 1, EUMGSequencePlayMode::Reverse, 8.0f, false);
+				}
+			}
+		}
+		else if (P2_INPUT_RIGHT && P2AcceptHorizontalInput)
+		{
+			P2AcceptHorizontalInput = false;
+			if (P2Side < 1 && !(P1Side == 1 && P2Side == 0))
+			{
+				P2Side++;
+				if (P2Side == 1)
+				{
+					StopAnimation(P2CToL);
+					StopAnimation(P2CToR);
+					PlayAnimation(P2CToR, 0.0f, 1, EUMGSequencePlayMode::Forward, 8.0f, false);
+				}
+				else if (P2Side == 0)
+				{
+					StopAnimation(P2CToL);
+					StopAnimation(P2CToR);
+					PlayAnimation(P2CToL, 0.0f, 1, EUMGSequencePlayMode::Reverse, 8.0f, false);
+				}
+			}
+		}
+		else if (!P2_INPUT_LEFT && !P2_INPUT_RIGHT)
+		{
+			P2AcceptHorizontalInput = true;
+		}
+	}
+
+	//Load character select if sides are chosen
+	if (P1_INPUT_ACCEPT && P1AcceptConfirmInput && !loading) 
+	{
+		if (Gamemode == "VS" && P1Side != 0 && P2Side != 0 && P1Side != P2Side || ((Gamemode == "CPU" || Gamemode == "Training") && P1Side != 0))
+		{
+			P1AcceptConfirmInput = false;
+			loading = true;
+			UGameplayStatics::OpenLevel(GetWorld(), "CharacterSelect");
+		}
+	}
+	else if (!P1_INPUT_ACCEPT) 
+	{
+		P1AcceptConfirmInput = true;
+	}
+
+	//Check for back input
+	if (P1_INPUT_BACK && P1AcceptBackInput && !loading)
+	{
+		P1AcceptBackInput = false;
+		StopAnimation(P1CToL);
+		StopAnimation(P1CToR);
+		StopAnimation(P2CToL);
+		StopAnimation(P2CToR);
+		SideSelectMenu->SetVisibility(ESlateVisibility::Hidden);
+		PlayAnimation(ResetControllerPositions, 0.0f, 1, EUMGSequencePlayMode::Reverse, 1.0f, false);
+		Gamemode = "";
+		P1Side = 0;
+		P2Side = 0;
+		menuIndex = 1;
+	}
+	else if (!P1_INPUT_BACK)
+	{
+		P1AcceptBackInput = true;
+	}
+}
+
+void UMainMenu::SetMarker(int currentOptionIndex) 
+{
+	switch (currentOptionIndex) 
+	{
+	case 0:
+		Cast<UCanvasPanelSlot>(Marker->Slot)->SetPosition(FVector2D(-500.0f, 50.0f));
+		break;
+	case 1:
+		Cast<UCanvasPanelSlot>(Marker->Slot)->SetPosition(FVector2D(-500.0f, 140.0f));
+		break;
+	case 2:
+		Cast<UCanvasPanelSlot>(Marker->Slot)->SetPosition(FVector2D(-500.0f, 230.0f));
+		break;
+	case 3:
+		Cast<UCanvasPanelSlot>(Marker->Slot)->SetPosition(FVector2D(-500.0f, 320.0f));
+		break;
+	case 4:
+		Cast<UCanvasPanelSlot>(Marker->Slot)->SetPosition(FVector2D(-500.0f, 410.0f));
+		break;
+	}
+}
---
+++ Source/BreakThrough/MainMenu.h
@@ -0,0 +1,117 @@
+// Copyright 2020 ShatterPoint Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "Components/PanelSlot.h"
+#include "Components/ProgressBar.h"
+#include "Components/CanvasPanelSlot.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/CanvasPanel.h"
+#include "Kismet/GameplayStatics.h"
+#include "Slate.h"
+#include "Math/UnrealMathUtility.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAKTHROUGH_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	//Widget Components
+	//Menu Panels
+	UPROPERTY(meta = (BindWidget))
+		class UCanvasPanel* MainMenu;
+	UPROPERTY(meta = (BindWidget))
+		class UCanvasPanel* PlayLocalMenu;
+	UPROPERTY(meta = (BindWidget))
+		class UCanvasPanel* OptionsMenu;
+	UPROPERTY(meta = (BindWidget))
+		class UCanvasPanel* SideSelectMenu;
+
+	//Images
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* Marker;
+	UPROPERTY(meta = (BindWidget))
+		class UImage* P1ControllerIcon;
+	UPROPERTY(meta = (BindWidget))
+		class UImage* P2ControllerIcon;
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* P1CPUText;
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* P2CPUText;
+
+	//Widget Animations
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* MarkerAnimation;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* FlowerAnimation;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* CloudAnimation;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* P1CToL;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* P1CToR;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* P2CToL;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* P2CToR;
+	UPROPERTY(meta = (BindWidgetAnim))
+		UWidgetAnimation* ResetControllerPositions;
+
+	//Local Variables
+	int menuIndex = 0;
+	int optionIndex = 0;
+	int P1Side;
+	int P2Side;
+
+	bool P1AcceptConfirmInput;
+	bool P1AcceptBackInput;
+	bool P1AcceptVerticalInput;
+	bool P1AcceptHorizontalInput;
+	bool P2AcceptHorizontalInput;
+	bool loading;
+
+	FString Gamemode;
+
+	//Input variables
+	//P1 inputs
+	float P1_HORIZONTAL_AXIS;
+	float P1_VERTICAL_AXIS;
+	bool P1_INPUT_DOWN;
+	bool P1_INPUT_UP;
+	bool P1_INPUT_LEFT;
+	bool P1_INPUT_RIGHT;
+	bool P1_INPUT_ACCEPT;
+	bool P1_INPUT_BACK;
+
+	//P2 inputs
+	float P2_HORIZONTAL_AXIS;
+	float P2_VERTICAL_AXIS;
+	bool P2_INPUT_LEFT;
+	bool P2_INPUT_RIGHT;
+
+	//Controllers
+	APlayerController* P1Controller;
+	APlayerController* P2Controller;
+
+	//Functions
+	void GetP1Inputs();
+	void GetP2Inputs();
+	void MainMenuInteractions();
+	void PlayLocalMenuInteractions();
+	void OptionsMenuInteractions();
+	void SideSelectMenuInteractions();
+	void SetMarker(int currentOptionIndex);
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+};
